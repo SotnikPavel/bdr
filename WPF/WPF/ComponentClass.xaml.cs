42c040c4c54f19226d02b1950fe252633e14f0c3
Pavel <pavel.s@cloudberrylab.com>
2017-04-20 09:09:03 +0300

    DBworker

--- WPF/WPF/ComponentClass.xaml.cs
+++ WPF/WPF/ComponentClass.xaml.cs
@@ -10,19 +10,28 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BDR
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для классы.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class классы : Window
     {
-        public MainWindow()
+        public классы()
         {
             InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
